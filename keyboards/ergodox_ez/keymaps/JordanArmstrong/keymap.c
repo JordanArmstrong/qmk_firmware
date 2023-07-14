43738cd0de9c3ca7c21b7448b160112e303269a7
Jordan Armstrong <jordan.armstrong@gmail.com>
2023-07-14 11:47:20 -0300

    Changes to my keymap to match upstream

--- keyboards/ergodox_ez/keymaps/JordanArmstrong/keymap.c
+++ keyboards/ergodox_ez/keymaps/JordanArmstrong/keymap.c
@@ -2,12 +2,12 @@
 #include "version.h"
 
 enum custom_keycodes {
-  RGB_SLD = EZ_SAFE_RANGE,
+  RGB_SLD = SAFE_RANGE,
   UK_UCIS
 };
 // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_unicode.md
 // Useful (Use UTF-16 codepoints): https://r12a.github.io/app-conversion/
-const qk_ucis_symbol_t ucis_symbol_table[] = UCIS_TABLE(
+const ucis_symbol_t ucis_symbol_table[] = UCIS_TABLE(
     UCIS_SYM("poop", 0x1F4A9),                // 💩
     UCIS_SYM("rofl", 0x1F923),                // 🤣
     UCIS_SYM("ca", 0x1F1E8, 0x1F1E6),       // 🇨🇦
@@ -44,21 +44,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [0] = LAYOUT_ergodox_pretty(
 LT(2,KC_EQUAL),     KC_1,    KC_2,        KC_3,        KC_4,                        KC_5, KC_DELETE,      KC_INSERT    , KC_6     , KC_7    , KC_8    , KC_9  , KC_0                 , KC_MINUS             ,
-        KC_TAB,     KC_Q,    KC_W,        KC_E,        KC_R,                        KC_T,     TG(1),      TG(1)        , KC_Y     , KC_U    , KC_I    , KC_O  , KC_P                 , KC_BSLASH            ,
-      KC_LCTRL,     KC_A,    KC_S,        KC_D,        KC_F,                        KC_G,                                KC_H     , KC_J    , KC_K    , KC_L  , LT(2,KC_SCOLON)      , MT(MOD_LGUI,KC_QUOTE),
-     KC_LSHIFT,     KC_Z,    KC_X,        KC_C,        KC_V,                        KC_B,   KC_HYPR,      KC_MEH       , KC_N     , KC_M    , KC_COMMA, KC_DOT, MT(MOD_RCTL,KC_SLASH), KC_RSHIFT            ,
-LT(1,KC_GRAVE), KC_QUOTE, KC_LALT, KC_LBRACKET, KC_RBRACKET,                                                                        KC_LEFT , KC_DOWN , KC_UP , KC_RIGHT             , MO(1)                ,
+        KC_TAB,     KC_Q,    KC_W,        KC_E,        KC_R,                        KC_T,     TG(1),      TG(1)        , KC_Y     , KC_U    , KC_I    , KC_O  , KC_P                 , KC_BSLS              ,
+       KC_LCTL,     KC_A,    KC_S,        KC_D,        KC_F,                        KC_G,                                KC_H     , KC_J    , KC_K    , KC_L  , LT(2,KC_SCLN)        , MT(MOD_LGUI,KC_QUOTE),
+       KC_LSFT,     KC_Z,    KC_X,        KC_C,        KC_V,                        KC_B,   KC_HYPR,      KC_MEH       , KC_N     , KC_M    , KC_COMMA, KC_DOT, MT(MOD_RCTL,KC_SLASH), KC_RSFT              ,
+LT(1,KC_GRAVE), KC_QUOTE, KC_LALT,     KC_LBRC,     KC_RBRC,                                                                        KC_LEFT , KC_DOWN , KC_UP , KC_RIGHT             , MO(1)                ,
 
                                                              MT(MOD_LALT,KC_APPLICATION),   KC_LGUI,      LSFT(KC_LALT), KC_ESCAPE,
                                                                                             KC_HOME,      KC_PGUP      ,
-                                                   KC_SPACE,                   KC_BSPACE,    KC_END,      KC_PGDOWN    , KC_TAB   , KC_ENTER
+                                                   KC_SPACE,                     KC_BSPC,    KC_END,      KC_PGDN      , KC_TAB   , KC_ENTER
 ),
 
 [1] = LAYOUT_ergodox_pretty(
 _______,   KC_F1,   KC_F2,       KC_F3,       KC_F4,    KC_F5, _______,      _______, KC_F6  , KC_F7  , KC_F8 , KC_F9, KC_F10   , KC_F11 ,
 _______, KC_EXLM,   KC_AT,     KC_LCBR,     KC_RCBR,  KC_PIPE, _______,      _______, KC_UP  , KC_7   , KC_8  , KC_9 , KC_ASTR  , KC_F12 ,
 _______, KC_HASH,  KC_DLR,     KC_LPRN,     KC_RPRN, KC_GRAVE,                        KC_DOWN, KC_4   , KC_5  , KC_6 , _______  , _______,
-_______, KC_PERC, KC_CIRC, KC_LBRACKET, KC_RBRACKET,  KC_TILD, _______,      _______, KC_AMPR, KC_1   , KC_2  , KC_3 , KC_BSLASH, _______,
+_______, KC_PERC, KC_CIRC,     KC_LBRC,     KC_RBRC,  KC_TILD, _______,      _______, KC_AMPR, KC_1   , KC_2  , KC_3 , KC_BSLS  , _______,
 _______, _______, _______,     _______,     _______,                                           _______, KC_DOT, KC_0 , KC_EQUAL , _______,
 
                                                       _______, _______,      _______, _______,
@@ -67,7 +67,7 @@ _______, _______, _______,     _______,     _______,                            
 ),
 
 [2] = LAYOUT_ergodox_pretty(
-_______, UC_M_LN,    UC_M_WC,    UC_M_MA,     _______, _______, _______,      RESET  , _______, _______            , _______          , _______        , _______            , _______            ,
+_______, UC_LINX,    UC_WINC,     UC_MAC,     _______, _______, _______,      QK_BOOT, _______, _______            , _______          , _______        , _______            , _______            ,
 _______, _______,    _______,   KC_MS_UP,     _______, _______, _______,      _______, _______, UK_UCIS            , _______          , _______        , _______            , _______            ,
 _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, _______,                        _______, _______            , _______          , _______        , _______            , KC_MEDIA_PLAY_PAUSE,
 _______, _______,    _______,    _______,     _______, _______, _______,      _______, _______, _______            , _______          , _______        , _______            , _______            ,
@@ -91,14 +91,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
     case UK_UCIS:
       if (record->event.pressed) {
-        qk_ucis_start();
+        ucis_start();
       }
       return false;
   }
   return true;
 }
 
-uint32_t layer_state_set_user(uint32_t state) {
+layer_state_t layer_state_set_user(layer_state_t state) {
 
   uint8_t layer = biton32(state);
 
