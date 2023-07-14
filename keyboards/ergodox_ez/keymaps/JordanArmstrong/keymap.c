a9122f3d37fdb7a628b132fd0cd4ad780e5e7982
Jordan Armstrong <jordan.armstrong@gmail.com>
2023-07-14 12:05:13 -0300

    Remove modifier I don't use

--- keyboards/ergodox_ez/keymaps/JordanArmstrong/keymap.c
+++ keyboards/ergodox_ez/keymaps/JordanArmstrong/keymap.c
@@ -30,11 +30,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
 [0] = LAYOUT_ergodox_pretty(
-LT(2,KC_EQUAL),     KC_1,    KC_2,        KC_3,        KC_4,                        KC_5, KC_DELETE,      KC_INSERT    , KC_6     , KC_7    , KC_8    , KC_9  , KC_0                 , KC_MINUS             ,
+      KC_EQUAL,     KC_1,    KC_2,        KC_3,        KC_4,                        KC_5, KC_DELETE,      KC_INSERT    , KC_6     , KC_7    , KC_8    , KC_9  , KC_0                 , KC_MINUS             ,
         KC_TAB,     KC_Q,    KC_W,        KC_E,        KC_R,                        KC_T,     TG(1),      TG(1)        , KC_Y     , KC_U    , KC_I    , KC_O  , KC_P                 , KC_BSLS              ,
        KC_LCTL,     KC_A,    KC_S,        KC_D,        KC_F,                        KC_G,                                KC_H     , KC_J    , KC_K    , KC_L  , LT(2,KC_SCLN)        , MT(MOD_LGUI,KC_QUOTE),
        KC_LSFT,     KC_Z,    KC_X,        KC_C,        KC_V,                        KC_B,   KC_HYPR,      KC_MEH       , KC_N     , KC_M    , KC_COMMA, KC_DOT, MT(MOD_RCTL,KC_SLASH), KC_RSFT              ,
-LT(1,KC_GRAVE), KC_QUOTE, KC_LALT,     KC_LBRC,     KC_RBRC,                                                                        KC_LEFT , KC_DOWN , KC_UP , KC_RIGHT             , MO(1)                ,
+      KC_GRAVE, KC_QUOTE, KC_LALT,     KC_LBRC,     KC_RBRC,                                                                        KC_LEFT , KC_DOWN , KC_UP , KC_RIGHT             , MO(1)                ,
 
                                                              MT(MOD_LALT,KC_APPLICATION),   KC_LGUI,      LSFT(KC_LALT), KC_ESCAPE,
                                                                                             KC_HOME,      KC_PGUP      ,
